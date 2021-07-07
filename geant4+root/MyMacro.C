03b0deb466058662a3d4d13213e6c80ba0df2efd
Go Iwai <go.iwai@kek.jp>
2021-07-07 14:31:12 +0900

    proactive use of auto operator

--- geant4+root/MyMacro.C
+++ geant4+root/MyMacro.C
@@ -4,22 +4,22 @@
 #include "TCanvas.h"
 #include "TFile.h"
 
-void MyMacro(const TString &ofile = "myfile.root") {
-  TH1D* h = new TH1D("hist", "My Histogram", 100, -5, 5);
+void MyMacro(const TString& ofile = "myfile.root") {
+  auto h = new TH1D("hist", "My Histogram", 100, -5, 5);
   h->FillRandom("gaus");
 
   Double_t x[] = {0, 1, 2, 3, 4};
   Double_t y[] = {0, 1, 8, 27, 64};
-  TGraph* g = new TGraph(5, x, y);
+  auto g = new TGraph(5, x, y);
 
-  TCanvas* cv = new TCanvas("cv", "Canvas example", 800, 1200);
+  auto cv = new TCanvas("cv", "Canvas example", 800, 1200);
   cv->Divide(1, 2);
   cv->cd(1);
   h->Draw();
   cv->cd(2);
   g->Draw("ALP");
 
-  TFile* f = new TFile(ofile.Data(), "RECREATE");
+  auto f = new TFile(ofile.Data(), "RECREATE");
   h->Write();
   g->Write();
   f->Close();
